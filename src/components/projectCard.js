a40a1a57ac61ec48b3f593910fbf384db11ef1b2
Nathaniel <nathanielmat2@gmail.com>
2019-10-29 22:05:50 +0530

    added projects page

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -3,14 +3,14 @@ import { Link } from "gatsby"
 import '../styles/projectCard.css'
 const projectCard=(props)=>(
     <div className="project-card-content">
-            <Link to="/">
+            <a href={props.plink}>
                 <h3 className="project-name">
                     {props.pname}
                 </h3>
                 <p className="project-info">
-                    Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor
+                    {props.pinfo}
                 </p>
-            </Link>
+            </a>
     </div>
 );
 
---
+++ src/pages/projects.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import ProjectCard from '../components/projectCard.js'
+
+const Achievements = () => (
+  <Layout>
+    <SEO title="Projects" />
+    <div className="projects" name="projects">
+        <h1>My Projects</h1>
+        <p>here are  a few of the projects that I've been working on</p>
+        <div className="project-card-container">
+          <ProjectCard pname="Portfolio WebPage" pinfo="A webpage to display my Information, Achievement and Projects" plink="/"/>
+          <ProjectCard pname="Bulk Mailer" pinfo="A web application to send mass E-mails and mass SMS's on a large scale" plink="https://github.com/sih-sosc"/>
+          <ProjectCard pname="Weather API page" pinfo="A web application to display the weather conditions of any location" plink="https://nathanielmathew.github.io/weather-api/"/>
+          <ProjectCard pname="Mozilla Sahyadri Website" pinfo="A website for the official Mozilla Campus Club of Sahyadri" plink="https://mozilla-sahyadri.netlify.com/"/>
+        </div>
+    </div>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default Achievements
