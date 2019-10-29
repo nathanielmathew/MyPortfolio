482bad8c56dfe8827854464e172c96f3dec21427
Nathaniel <nathanielmat2@gmail.com>
2019-10-29 22:33:04 +0530

    added projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,6 +3,7 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import '../styles/projects.css'
 import ProjectCard from '../components/projectCard.js'
 
 const Achievements = () => (
