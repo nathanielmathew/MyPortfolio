249e8c1923841168c0aa7a7e430ce705b4c343e3
Nathaniel <nathanielmat2@gmail.com>
2019-12-06 21:02:00 +0530

    added gatsby-image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Img from 'gatsby-image'
-import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { graphql } from 'gatsby'
