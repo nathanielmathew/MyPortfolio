c8b29cee1c9d2577f41f2289eb3b15790db18ccd
Nathaniel <nathanielmat2@gmail.com>
2020-01-09 22:38:36 +0530

    add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#1d0a42`,
         theme_color: `#1d0a42`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
