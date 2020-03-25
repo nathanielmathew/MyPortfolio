3db366ad0f71784f1bf4a035e44f138e8be26927
Nathaniel <nathanielmat2@gmail.com>
2020-03-25 21:48:31 +0530

    added interests

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,38 +7,63 @@ import '../styles/index.css'
 const IndexPage = props => (
   <Layout>
     <SEO title="Home" />
-    <div className="intro-container dark">
-      <div className="intro" data-sal="fade" data-sal-duration="1000">
-        <h1>Hello there!</h1>
-        <p>I'm <strong>Nathaniel Ryan M</strong>,<br />
+    <div className="intro-container frame dark">
+      <div className="intro">
+        <h1 data-sal="fade" data-sal-duration="500">Hello there!</h1>
+        <p data-sal="fade" data-sal-duration="1000">I'm <strong>Nathaniel Ryan M</strong>,<br />
         a 3rd Year Computer Science and Engineering Student at Sahyadri College of Engineering &amp; Management</p>
       </div>
-      <div className="avatar" data-sal="fade" data-sal-duration="1000">
+      <div className="avatar" data-sal="slide-up" data-sal-duration="1000">
         <Img fluid={props.data.imageOne.childImageSharp.fluid}
         />
       </div>
     </div>
-    <div className="about-container">
-      <div className="about" data-sal="fade" data-sal-duration="1000"  >
-        <h1>About Me</h1>
-        <p>I'm an Engineering Student Developer from Mangalore, with a passion for <b>Front-End Web Development</b>, <b>UI/UX Design</b> and <b>Graphic Design</b>.<br />
+    <div className="about-container frame">
+      <div className="about">
+        <h1 data-sal="fade" data-sal-duration="500">About Me</h1>
+        <p data-sal="fade" data-sal-duration="1000">I'm an Engineering Student Developer from Mangalore, with a passion for <b>Front-End Web Development</b>, <b>UI/UX Design</b> and <b>Graphic Design</b>.<br />
         I'm also a Music &amp; Art enthusiast and a Guitarist by hobby</p>
       </div>
-      <div className="about-img" data-sal="fade" data-sal-duration="1000">
+      <div className="about-img" data-sal="slide-up" data-sal-duration="1000">
       <Img fluid={props.data.about.childImageSharp.fluid}
       />
       </div>
     </div>
-    <div className="contact-container dark">
-      <div className="contact" data-sal="fade" data-sal-duration="1000">
-        <h1>Get in Touch!</h1>
-        <p>Be sure to contact me for exciting Collaboration opportunities and more information:</p>
-        <p>
+
+    <div className="interests-container frame dark">
+        <h1 align="center" data-sal="fade" data-sal-duration="500">Areas of Interest</h1>
+        <div className="interests" data-sal="slide-up" data-sal-duration="1000">
+
+        <div className="interest-card">
+          <Img className="icon" fluid={props.data.webDev.childImageSharp.fluid}
+          />  
+          <p>Web Development</p>
+        </div>
+
+        <div className="interest-card">
+          <Img className="icon" fluid={props.data.design.childImageSharp.fluid}
+          />  
+          <p>Graphic Design</p>
+        </div>
+
+        <div className="interest-card">
+          <Img className="icon" fluid={props.data.gameDev.childImageSharp.fluid}
+          />  
+          <p>Game Development</p>
+        </div>
+      </div>
+    </div>
+
+    <div className="contact-container frame">
+      <div className="contact">
+        <h1 data-sal="fade" data-sal-duration="500">Get in Touch!</h1>
+        <p data-sal="fade" data-sal-duration="1000">Be sure to contact me for exciting Collaboration opportunities and more information:</p>
+        <p data-sal="fade" data-sal-duration="1000">
         Drop me an e-mail at<br /> <a href="mailto:nathanielmat2@gmail.com"><b>nathanielmat2@gmail.com</b></a><br />
         Or give me a call/text at<br /> <a href="tel:+91 8088734430"><b>+91 8088734430</b></a>
         </p>
       </div>
-      <div className="contact-img" data-sal="fade" data-sal-duration="1000">
+      <div className="contact-img" data-sal="slide-up" data-sal-duration="1000">
       <Img fluid={props.data.MyFirst.childImageSharp.fluid}
       />
       </div>
@@ -65,6 +90,15 @@ export const pageQuery = graphql`
       ...firstSiteImages
     }
     imageOne: file(relativePath: { eq: "images/nat.png" }) {
+      ...firstSiteImages
+    }
+    webDev: file(relativePath: { eq: "images/web.png" }) {
+      ...firstSiteImages
+    }
+    design: file(relativePath: { eq: "images/design.png" }) {
+      ...firstSiteImages
+    }
+    gameDev: file(relativePath: { eq: "images/game.png" }) {
       ...firstSiteImages
     }
   }
