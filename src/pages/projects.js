8fe4308a9cb076f3c9683ce8c3dd9daf6811b52d
Nathaniel <nathanielmat2@gmail.com>
2020-08-24 21:22:07 +0530

    fixed graphql warnings

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Img from 'gatsby-image'
+import { graphql } from 'gatsby'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import '../styles/projects.css'
